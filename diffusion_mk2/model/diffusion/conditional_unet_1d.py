6098e690040ac79070bf45b935fb1630e4f6c5fd
mengo <riccardo.mengozzi3@studio.unibo.it>
2025-06-26 13:55:28 +0200

    added transformer model and splitted conv1d

--- diffusion_mk2/model/diffusion/conditional_unet_1d.py
+++ diffusion_mk2/model/diffusion/conditional_unet_1d.py
@@ -2,59 +2,12 @@
 https://github.com/real-stanford/diffusion_policy
 """
 
-import math, torch 
+import torch 
 import torch.nn as nn
 from typing import Union
+from diffusion_mk2.model.diffusion.positional_embedding import SinusoidalPosEmb
+from diffusion_mk2.model.diffusion.conv1d_components import Conv1dBlock, Downsample1d, Upsample1d
 
-
-class SinusoidalPosEmb(nn.Module):
-    def __init__(self, dim):
-        super().__init__()
-        self.dim = dim
-
-    def forward(self, x):
-        device = x.device
-        half_dim = self.dim // 2
-        emb = math.log(10000) / (half_dim - 1)
-        emb = torch.exp(torch.arange(half_dim, device=device) * -emb)
-        emb = x[:, None] * emb[None, :]
-        emb = torch.cat((emb.sin(), emb.cos()), dim=-1)
-        return emb
-
-
-class Downsample1d(nn.Module):
-    def __init__(self, dim):
-        super().__init__()
-        self.conv = nn.Conv1d(dim, dim, 3, 2, 1)
-
-    def forward(self, x):
-        return self.conv(x)
-
-class Upsample1d(nn.Module):
-    def __init__(self, dim):
-        super().__init__()
-        self.conv = nn.ConvTranspose1d(dim, dim, 4, 2, 1)
-
-    def forward(self, x):
-        return self.conv(x)
-
-
-class Conv1dBlock(nn.Module):
-    '''
-        Conv1d --> GroupNorm --> Mish
-    '''
-
-    def __init__(self, inp_channels, out_channels, kernel_size, n_groups=8):
-        super().__init__()
-
-        self.block = nn.Sequential(
-            nn.Conv1d(inp_channels, out_channels, kernel_size, padding=kernel_size // 2),
-            nn.GroupNorm(n_groups, out_channels),
-            nn.Mish(),
-        )
-
-    def forward(self, x):
-        return self.block(x)
 
 
 class ConditionalResidualBlock1D(nn.Module):
@@ -252,9 +205,9 @@ class ConditionalUnet1D(nn.Module):
 
 if __name__ == "__main__":
     # Test
-    obs_dim = 5
+    obs_dim = 95
     obs_horizon = 2
-    action_dim = 2
+    action_dim = 5
     pred_horizon = 16
 
 
