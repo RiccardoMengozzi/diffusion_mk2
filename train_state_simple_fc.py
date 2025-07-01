ebc3000a76ac338baedb4c027d6e331e036bf607
Riccardo Mengozzi <riccardo.mengozzi3@studio.unibo.it>
2025-07-01 13:31:27 +0200

    fix

--- train_state_simple_fc.py
+++ train_state_simple_fc.py
@@ -14,12 +14,12 @@ np.set_printoptions(precision=4,    # number of decimal places
 
 PROJECT_DIR = os.path.dirname(__file__)
 LOG_INTERVAL = 50
-CHECKPOINT_INTERVAL = 10000
+CHECKPOINT_INTERVAL = 30000
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 config = dict(
-    batch_size=128,
-    epochs=100000,
+    batch_size=256,
+    epochs=300000,
     lr=5e-4,
     hidden_dim=256,
     dim_points=2,
