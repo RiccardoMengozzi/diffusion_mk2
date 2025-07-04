abfdca0d324522a7608735753b89ae6725d195be
mengo <riccardo.mengozzi3@studio.unibo.it>
2025-07-04 17:40:57 +0200

    fixes

--- diffusion_mk2/scripts/creating_traj_pred_states_dataset.py
+++ diffusion_mk2/scripts/creating_traj_pred_states_dataset.py
@@ -9,8 +9,8 @@ import json
 
 project_dir   = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 # npz_filename  = os.path.join(project_dir, "npz_data", "teleop_pushing_dataset.npz")
-filename  = os.path.join(project_dir, "json_data", "combined_dataset_test_first.jsonl")
-zarr_filename = os.path.join(project_dir, "zarr_data/pushing_dataset_test_first.zarr.zip")
+filename  = os.path.join(project_dir, "json_data", "test.jsonl")
+zarr_filename = os.path.join(project_dir, "zarr_data/test.zarr.zip")
 
 # ------------------------------------------------------------
 
