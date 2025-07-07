fixed totally normalization, adding also scaling of ee_state and action between -1 and 1

diffusion_mk2/dataset/pusht_state_dataset.py
@@ -119,7 +119,6 @@ class PushTStateDataset(torch.utils.data.Dataset):
         self.obs_ee_dim = obs_ee_dim
         self.obs_dlo_dim = obs_dlo_dim
         self.obs_target_dim = obs_target_dim
-
         # read from zarr dataset
         dataset_root = zarr.open(dataset_path, 'r')
 
@@ -151,13 +150,18 @@ class PushTStateDataset(torch.utils.data.Dataset):
         #     stats[key] = get_data_stats(data)
         #     normalized_train_data[key] = normalize_data(data, stats[key])
 
-        normalized_train_data = self.normalize_data(train_data)
+        self.initial_shapes_processor = None
+        self.final_shapes_processor = None
+        self.ee_states_processor = None
+        self.actions_processor = None
+
 
         self.indices = indices
-        self.normalized_train_data = normalized_train_data
         self.pred_horizon = pred_horizon
         self.action_horizon = action_horizon
         self.obs_horizon = obs_horizon
+
+        self.normalized_train_data = self.normalize_data(train_data)
 
 
     def normalize_data(self, data):
@@ -172,31 +176,27 @@ class PushTStateDataset(torch.utils.data.Dataset):
         initial_shapes = states[:, initial_shape_range[0]:initial_shape_range[1]].reshape(-1, self.obs_dlo_dim // 3, 3)
         final_shapes = states[:, final_shape_range[0]:final_shape_range[1]].reshape(-1, self.obs_target_dim // 3, 3)
 
-        initial_shapes_processor = DloDataProcessor(initial_shapes)
-        final_shapes_processor = DloDataProcessor(final_shapes)
-        ee_states_processor = EEStateDataProcessor(ee_states)
-        actions_processor = ActionDataProcessor(actions, initial_shapes.shape[1],  is_first_idx=False, is_last_gripper=True)
+        self.initial_shapes_processor = DloDataProcessor(initial_shapes)
+        self.final_shapes_processor = DloDataProcessor(final_shapes)
+        self.ee_states_processor = EEStateDataProcessor(ee_states)
+        self.actions_processor = ActionDataProcessor(actions, initial_shapes.shape[1],  is_first_idx=False, is_last_gripper=True)
 
-        norm_factors = initial_shapes_processor.compute_normalize_factors_arrays()
+        norm_factors = self.initial_shapes_processor.compute_normalize_factors_arrays()
 
 
-        initial_shapes_processor.set_normalize_factors_arrays(*norm_factors)
-        final_shapes_processor.set_normalize_factors_arrays(*norm_factors)
-        ee_states_processor.set_normalize_factors_arrays(*norm_factors)
-        actions_processor.set_normalize_factors_arrays(*norm_factors)
+        self.initial_shapes_processor.set_normalize_factors_arrays(*norm_factors)
+        self.final_shapes_processor.set_normalize_factors_arrays(*norm_factors)
+        self.ee_states_processor.set_normalize_factors_arrays(*norm_factors)
+        self.actions_processor.set_normalize_factors_arrays(*norm_factors)
 
-        initial_shapes_n, init_shapes_nans = initial_shapes_processor.preprocess()
-        final_shapes_n, final_shapes_nans = final_shapes_processor.preprocess()
-        ee_states_n = ee_states_processor.preprocess()
-        actions_n = actions_processor.preprocess()
+        initial_shapes_n, init_shapes_nans = self.initial_shapes_processor.preprocess()
+        final_shapes_n, final_shapes_nans = self.final_shapes_processor.preprocess()
+        ee_states_n = self.ee_states_processor.preprocess()
+        actions_n = self.actions_processor.preprocess()
 
         initial_shapes_n = initial_shapes_n.reshape(-1, self.obs_dlo_dim)
         final_shapes_n = final_shapes_n.reshape(-1, self.obs_target_dim)
         ee_states_n = ee_states_n.reshape(-1, self.obs_ee_dim)
-
-        print("initial_shapes_n shape:", initial_shapes_n.shape)
-        print("final_shapes_n shape:", final_shapes_n.shape)
-        print("ee_states_n shape:", ee_states_n.shape)
 
         states_n = np.concatenate([
             ee_states_n,

diffusion_mk2/envs/dlo_shaping/shaping_dataset_generator.py
@@ -184,6 +184,7 @@ class GripperDataGenerator():
         self.action_from_grasp_to_release = None
         self.idx = None  # Index of the particle to grasp
         self.previous_obs_ee = None  # Previous end effector observation
+        self.is_first_episode_step = True
 
     def _step(self):
         start_time = time.time()
@@ -206,6 +207,7 @@ class GripperDataGenerator():
 
     def reset(self, reset_rope_position=False):
         """Reset the environment for a new episode."""
+        self.is_first_episode_step = True
         if reset_rope_position:
             # Resettin completely the rope
             self.reset_rope_pose()
@@ -281,8 +283,9 @@ class GripperDataGenerator():
         # print("Action:", action)
 
         # simpy save previous obs as action:
-        if self.previous_obs_ee is None:
+        if self.previous_obs_ee is None or self.is_first_episode_step:
             self.previous_obs_ee = current_obs_ee
+            self.is_first_episode_step = False
             return np.zeros(5)
 
         px, py, pz, ptheta, pfinger_qpos = self.previous_obs_ee

diffusion_mk2/model/normalization.py
@@ -6,9 +6,10 @@ class ActionDataProcessor():
         self.num_points = num_points
         self.cs0_list = None
         self.csR_list = None
+        self.scale_min = None
+        self.scale_max = None
         self.is_first_idx = is_first_idx  # Whether the first component is an index
         self.is_last_gripper = is_last_gripper  # Whether the last component is the
-        print("self.is_first_idx:", self.is_first_idx, "self.is_last_gripper:", self.is_last_gripper)
 
     def set_normalize_factors_arrays(self, cs0_list, csR_list):
         self.cs0_list = cs0_list
@@ -18,9 +19,7 @@ class ActionDataProcessor():
         # Extract components - works for both 2D and 3D
         pos_start_idx, pos_last_idx = 0, -1
         if self.is_first_idx:
-            idx = action[0]
-            # Normalize index
-            idx_n = idx / (self.num_points - 1.0)
+            idx = action[0]  # First component is an index
             pos_start_idx = 1
         if self.is_last_gripper:
             gripper_state = action[-1]  # Last component is gripper state
@@ -29,7 +28,6 @@ class ActionDataProcessor():
         pos = action[pos_start_idx:pos_last_idx]  # All spatial components (dx, dy) or (dx, dy, dz)
             
         dtheta = action[pos_last_idx+1] 
-        print(f"pos_first_idx: {pos_start_idx}, pos_last_idx: {pos_last_idx}, pos_shape: {pos.shape}, self.is_first_idx: {self.is_first_idx}, self.is_last_gripper: {self.is_last_gripper}")
 
         
         # Normalize spatial components using rotation matrix
@@ -40,21 +38,36 @@ class ActionDataProcessor():
         
         # Reconstruct normalized action
         if self.is_first_idx and self.is_last_gripper:
-            return np.concatenate([[idx_n], pos_n, [theta_n], [gripper_state]])
+            return np.concatenate([[idx], pos_n, [theta_n], [gripper_state]])
         elif self.is_first_idx:
-            return np.concatenate([[idx_n], pos_n, [theta_n]])
+            return np.concatenate([[idx], pos_n, [theta_n]])
         elif self.is_last_gripper:
             return np.concatenate([pos_n, [theta_n], [gripper_state]])
         else:
             return np.concatenate([pos_n, [theta_n]])
 
         
-    def denormalize(self, action_n, csR):
+    def denormalize(self, action_n, descale, csR=None, idx=None):
+        """
+        If you give cs0 and csR, they will be used for denormalization.
+        otherwise u can give the idx, that will be used to get the cs0 and csR from the lists.
+        """
+        if csR is None:
+            if self.csR_list is None:
+                raise ValueError("Normalization factors not set. Call set_normalize_factors_arrays() first.")
+            if idx is None:
+                raise ValueError("Either cs0 and csR must be provided or an index must be given.")
+            csR = self.csR_list[idx]
+
+        if descale:
+            if self.scale_min is None or self.scale_max is None:
+                raise ValueError("scaling range not set!!!")
+            action_n = self.descale(action_n, self.scale_min, self.scale_max)
+
         pos_start_idx, pos_last_idx = 0, -1
         if self.is_first_idx:
             idx_n = action_n[0]
             # denormalize index
-            idx = idx_n * (self.num_points - 1.0)
             pos_start_idx = 1
         if self.is_last_gripper:
             gripper_state = action_n[-1]  # Last component is gripper state
@@ -72,31 +85,69 @@ class ActionDataProcessor():
         
         # Reconstruct denormalized action
         if self.is_first_idx and self.is_last_gripper:
-            return np.concatenate([[idx], pos, [theta], [gripper_state]])
+            return np.concatenate([[idx_n], pos, [theta], [gripper_state]])
         elif self.is_first_idx:
-            return np.concatenate([[idx], pos, [theta]])
+            return np.concatenate([[idx_n], pos, [theta]])
         elif self.is_last_gripper:
             return np.concatenate([pos, [theta], [gripper_state]])
         else:
             return np.concatenate([pos, [theta]])
 
 
+    def scale(self, action, min, max):
+
+        # Scale to [0, 1]: (data - min) / (max - min)
+        scaled_action = (action - min) / (max - min)
+        
+        # Convert to [-1, 1]: [0, 1] -> [-1, 1]
+        scaled_action = scaled_action * 2 - 1
+
+        return scaled_action
+
+    def descale(self, action_scaled, min=None, max=None):
+        if min is None or max is None:
+            if self.scale_min is None or self.scale_max is None:
+                raise ValueError("scaling range not set!!!")
+            min, max = self.scale_min, self.scale_max
+        # Extract components
+        # Convert from [-1, 1] to [0, 1]
+        action = (action_scaled + 1) / 2
+        
+        # Scale back to original range: [0, 1] -> [min, max]
+        action = action * (max - min) + min
+        return action
+
     def preprocess(self):
         if self.cs0_list is None or self.csR_list is None:
             raise ValueError("Normalization factors not set. Call set_normalize_factors_arrays() first.")
-        
+
         processed_action_data = []
         for action, cs0, csR in zip(self.action_data, self.cs0_list, self.csR_list):
             action_n = self.normalize(action, csR)  # Note: removed cs0 parameter as it wasn't used
             processed_action_data.append(action_n)
-        
-        return np.array(processed_action_data)
+
+        processed_action_data = np.array(processed_action_data)
+        self.scale_min = np.min(processed_action_data, axis=0)
+        self.scale_max = np.max(processed_action_data, axis=0)
+
+        print("action_norm_min_max:", self.scale_min, self.scale_max)
+
+        scaled_processed_action_data = []
+        for action in processed_action_data:
+            action = self.scale(action, self.scale_min, self.scale_max)
+            scaled_processed_action_data.append(action)
+
+        return np.array(scaled_processed_action_data)
 
 class EEStateDataProcessor():
     def __init__(self, ee_state_data):
         self.ee_state_data = ee_state_data
         self.cs0_list = None
         self.csR_list = None
+        self.scale_min = None
+        self.scale_max = None
+
+ 
 
     def set_normalize_factors_arrays(self, cs0_list, csR_list):
         self.cs0_list = cs0_list
@@ -119,7 +170,24 @@ class EEStateDataProcessor():
         return np.concatenate([pos_n, [theta_n], [gripper_state]])  # Keep gripper state unchanged
 
         
-    def denormalize(self, ee_state_n, cs0, csR):
+    def denormalize(self, ee_state_n, descale, cs0=None, csR=None, idx=None):
+        """
+        If you give cs0 and csR, they will be used for denormalization.
+        otherwise u can give the idx, that will be used to get the cs0 and csR from the lists.
+        """
+        if cs0 is None or csR is None:
+            if self.cs0_list is None or self.csR_list is None:
+                raise ValueError("Normalization factors not set. Call set_normalize_factors_arrays() first.")
+            if idx is None:
+                raise ValueError("Either cs0 and csR must be provided or an index must be given.")
+            cs0 = self.cs0_list[idx]
+            csR = self.csR_list[idx]
+
+        if descale:
+            if self.scale_min is None or self.scale_max is None:
+                raise ValueError("scaling range not set!!!")
+            ee_state_n = self.descale(ee_state_n, self.scale_min, self.scale_max)
+
         # Extract components
         pos_n = ee_state_n[0:-2]  # All spatial components
         theta_n = ee_state_n[-2]
@@ -135,17 +203,50 @@ class EEStateDataProcessor():
         
         # Reconstruct denormalized action
         return np.concatenate([pos, [theta], [gripper_state]])  # Keep gripper state unchanged
+    
+    def scale(self, ee_state, min, max):
+        # Scale to [0, 1]: (data - min) / (max - min)
+        scaled_ee_state = (ee_state - min) / (max - min)
+        # Convert to [-1, 1]: [0, 1] -> [-1, 1]
+        scaled_ee_state = scaled_ee_state * 2 - 1
+
+        return scaled_ee_state
+
+    def descale(self, ee_state_scaled, min=None, max=None):
+        if min is None or max is None:
+            if self.scale_min is None or self.scale_max is None:
+                raise ValueError("scaling range not set!!!")
+            min, max = self.scale_min, self.scale_max
+        # Extract components
+        # Convert from [-1, 1] to [0, 1]
+        ee_state = (ee_state_scaled + 1) / 2
+        
+        # Scale back to original range: [0, 1] -> [min, max]
+        ee_state = ee_state * (max - min) + min
+        return ee_state
 
     def preprocess(self):
         if self.cs0_list is None or self.csR_list is None:
             raise ValueError("Normalization factors not set. Call set_normalize_factors_arrays() first.")
-        
+
         processed_ee_state_data = []
         for ee_state, cs0, csR in zip(self.ee_state_data, self.cs0_list, self.csR_list):
             ee_state_n = self.normalize(ee_state, cs0, csR)  # Note: removed cs0 parameter as it wasn't used
             processed_ee_state_data.append(ee_state_n)
-        
-        return np.array(processed_ee_state_data)
+
+        processed_ee_state_data = np.array(processed_ee_state_data)
+        self.scale_min = np.min(processed_ee_state_data, axis=0)
+        self.scale_max = np.max(processed_ee_state_data, axis=0)
+
+        print("ee_norm_min_max:", self.scale_min, self.scale_max)
+
+        scaled_processed_ee_state_data = []
+        for ee_state in processed_ee_state_data:
+            ee_state = self.scale(ee_state, self.scale_min, self.scale_max)
+            scaled_processed_ee_state_data.append(ee_state)
+
+        return np.array(scaled_processed_ee_state_data)
+    
     
 
 class DloDataProcessor():
@@ -155,15 +256,82 @@ class DloDataProcessor():
         self.csR_list = None
 
     def _compute_normalize_factors(self, dlo):
-        # Works automatically for any number of spatial dimensions
         cs0 = np.mean(dlo, axis=0, keepdims=True)
         dlo_centered = dlo - cs0
+        
+        # Check DLO linearity
+        dlo_range = np.max(dlo, axis=0) - np.min(dlo, axis=0)
+        is_linear = self._is_dlo_linear(dlo_centered, dlo_range)
+        
+        if is_linear:
+            # For linear DLO, use the DLO's main axis as primary axis
+            csR = self._compute_linear_coordinate_system(dlo_centered)
+        else:
+            # For non-linear DLO, use PCA
+            csR = self._compute_pca_coordinate_system(dlo_centered)
+        
+        return cs0, csR
+
+    def _is_dlo_linear(self, dlo_centered, dlo_range, linear_threshold=1000):
+        """Check if DLO is approximately linear."""
+        # Method 1: Check if one dimension dominates
+        max_range = np.max(dlo_range)
+        other_ranges = dlo_range[dlo_range != max_range]
+        ratio = max_range / (np.mean(other_ranges) + 1e-6)
+        
+        if ratio > 10:  # One dimension is much larger
+            return True
+        
+        # Method 2: Check PCA condition number
+        cov = dlo_centered.T @ dlo_centered
+        eigval = np.linalg.eigvals(cov)
+        condition_number = eigval.max() / (eigval.min() + 1e-12)
+        
+        return condition_number > linear_threshold  # Threshold for linearity, e.g., 1000
+
+    def _compute_linear_coordinate_system(self, dlo_centered):
+        """Compute coordinate system for linear DLO."""
+        # Create coordinate system aligned with main axis
+        csR = np.eye(3)
+        
+        # Compute actual direction of the DLO
+        first_point = dlo_centered[0]
+        last_point = dlo_centered[-1]
+        direction = last_point - first_point
+        direction = direction / (np.linalg.norm(direction) + 1e-6)
+        
+        # Align first axis with DLO direction
+        csR[0] = direction
+        
+        # Create orthogonal axes
+        if abs(direction[2]) < 0.9:  # Not aligned with Z
+            csR[1] = np.cross(direction, [0, 0, -1]) #-1 otherwise z becomes negative
+        else:  # Aligned with Z, use X
+            csR[1] = np.cross(direction, [1, 0, 0])
+        
+        csR[1] = csR[1] / (np.linalg.norm(csR[1]) + 1e-6)
+        csR[2] = np.cross(csR[0], csR[1])
+        
+        return csR
+
+    def _compute_pca_coordinate_system(self, dlo_centered):
+        """Compute PCA-based coordinate system."""
         cov = dlo_centered.T @ dlo_centered
         eigval, eigvec = np.linalg.eig(cov)
-        # Sort eigenvalues in descending order and arrange eigenvectors accordingly
+        
         sorted_indices = np.argsort(eigval)[::-1]
         csR = eigvec[:, sorted_indices].T
-        return cs0, csR
+        
+        # Anti-flip adjustments
+        if np.linalg.det(csR) < 0:
+            csR[-1, :] *= -1
+        
+        for i in range(csR.shape[0]):
+            j = np.argmax(np.abs(csR[i, :]))
+            if csR[i, j] < 0:
+                csR[i, :] *= -1
+        
+        return csR
     
     def compute_normalize_factors_arrays(self):
         cs0_list = []
@@ -172,7 +340,24 @@ class DloDataProcessor():
             cs0, csR = self._compute_normalize_factors(dlo)
             cs0_list.append(cs0)
             csR_list.append(csR)
-        return np.array(cs0_list), np.array(csR_list)
+        cs0_arr = np.vstack(cs0_list)
+        csR_arr = np.stack(csR_list)
+
+
+        # Step 2: enforce temporal sign consistency using frame 0 as reference
+        R_ref = csR_arr[0].copy()
+        for t in range(1, len(csR_arr)):
+            R_cur = csR_arr[t]
+            for k in range(R_cur.shape[0]):
+                # if current axis is more opposite than aligned, flip sign
+                if np.dot(R_ref[k], R_cur[k]) < 0:
+                    R_cur[k] *= -1
+            csR_arr[t] = R_cur
+
+        self.cs0_list = cs0_arr
+        self.csR_list = csR_arr
+        return cs0_arr, csR_arr
+
 
     def set_normalize_factors_arrays(self, cs0_list, csR_list):
         self.cs0_list = cs0_list
@@ -192,7 +377,19 @@ class DloDataProcessor():
         dlo_n = (csR @ (dlo - cs0).T).T
         return dlo_n
             
-    def denormalize(self, dlo, cs0, csR):
+    def denormalize(self, dlo, cs0=None, csR=None, idx=None):
+        """
+        If you give cs0 and csR, they will be used for denormalization.
+        otherwise u can give the idx, that will be used to get the cs0 and csR from the lists.
+        """
+        if cs0 is None or csR is None:
+            if self.cs0_list is None or self.csR_list is None:
+                raise ValueError("Normalization factors not set. Call set_normalize_factors_arrays() first.")
+            if idx is None:
+                raise ValueError("Either cs0 and csR must be provided or an index must be given.")
+            cs0 = self.cs0_list[idx]
+            csR = self.csR_list[idx]
+
         # Matrix operations work for any number of dimensions
         dlo = dlo.squeeze()  # Ensure dlo is 2D
         dlo_dn = (csR.T @ dlo.T).T + cs0